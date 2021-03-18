41f3453a3838b2d2a1068a6b51368d127adfa9d9
kriseiya <>
2021-03-18 19:47:00 -0400

    clean comments

--- noise.py
+++ noise.py
@@ -5,8 +5,4 @@ def add_gauss(image, std=0.1):
     return image + np.random.normal(size=image.shape) * std
 
 def add_poisson(image, lambd=70.):
-    # image = image*255
-    # lambd = len(np.unique(image.astype(np.uint8)))
-    # print(lambd)
-    # lambd = 2 ** np.ceil(np.log2(lambd))
     return np.random.poisson(image * lambd) / float(lambd)
