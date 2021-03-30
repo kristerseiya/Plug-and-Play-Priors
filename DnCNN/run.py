5ef89fde4dd4a31bba0e1841ef0d65f3f36840b9
kristerseiya <>
2021-03-30 14:32:54 -0400

    removed if __name__..

--- DnCNN/run.py
+++ DnCNN/run.py
@@ -4,6 +4,9 @@ import torch.nn.functional as F
 import numpy as np
 import torch
 import os
+from PIL import Image
+
+from . import data
 
 def train_single_epoch(model, optimizer, train_loader, noise_lvl):
 
@@ -25,13 +28,13 @@ def train_single_epoch(model, optimizer, train_loader, noise_lvl):
     return total_loss / float(dataset_size)
 
 @torch.no_grad()
-def validate(model, val_loader, noise_lvl):
+def test(model, test_loader, noise_lvl):
 
-    dataset_size = len(val_loader.dataset)
+    dataset_size = len(test_loader.dataset)
 
     total_loss = 0.
     model.eval()
-    for images in val_loader:
+    for images in test_loader:
         images = images.to(model.device)
         noise = torch.randn_like(images) * noise_lvl / 255.
         noisy = images + noise
@@ -62,7 +65,7 @@ def train(model, optimizer, max_epoch, train_loader, noise_lvl,
 
         if validation is not None:
 
-            log[e, 1] = validate(model, validation, noise_lvl)
+            log[e, 1] = test(model, validation, noise_lvl)
 
             print('Val Loss: {:.3f}'.format(log[e, 1]))
 
@@ -79,10 +82,11 @@ def train(model, optimizer, max_epoch, train_loader, noise_lvl,
                     return log[0:e, :]
 
     return log
-    
+
 
 @torch.no_grad()
 def inference(model, image):
+    model.eval()
     transform = data.get_transform('test')
     x = transform(image)
     x = x.unsqueeze(0)
@@ -90,38 +94,7 @@ def inference(model, image):
     x = model(x)
     x = x.squeeze(0)
     x = x.cpu().numpy()
-    x = x.transpose([2, 1, 0])
+    x = x.transpose([1, 2, 0])
     x = x.squeeze(-1)
     x = np.clip(x, 0, 1) * 255
     return Image.fromarray(x.astype(np.uint8), 'L')
-
-if __name__ == '__main__':
-
-    import argparse
-    from torch.utils.data import DataLoader
-    from torch.optim import Adam
-
-    from . import data
-    from . import model
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=None)
-    parser.add_argument('--data_dir', type=str, required=True)
-    parser.add_argument('--n_epoch', type=int, required=True)
-    parser.add_argument('--save', type=str, required=True)
-    args = parser.parse_args()
-
-    dataset = data.ImageDataset(args.data_dir, 'none')
-    trainset, valset, testset = dataset.split(0.7, 0.1, 0.2)
-    trainloader = DataLoader(trainset, batch_size=64, shuffle=True, drop_last=True)
-    valloader = DataLoader(valset, batch_size=64, shuffle=False)
-    testloader = DataLoader(testset, batch_size=64, shuffle=False)
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    net = model.DnCNN().move(device)
-    if args.weights != None:
-        net.load_state_dict(torch.load(args.weights, map_location=device))
-    optimizer = Adam(net.parameters(), lr=1e-4)
-    train(net, optimizer, args.n_epoch, trainloader, 40, valloader, args.save, -1)
-
-    torch.save(net.state_dict(), os.path.join(args.save, 'final.pth'))
