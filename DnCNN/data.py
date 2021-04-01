094b50a4a28f32d195826b94025914340ab27a64
kristerseiya <>
2021-03-31 23:31:35 -0400

    fix crop & crop selection

--- DnCNN/data.py
+++ DnCNN/data.py
@@ -9,16 +9,14 @@ import numpy as np
 
 def get_transform(mode):
     if mode == 'train':
-        transform = transforms.Compose([transforms.RandomCrop((50, 50)),
-                                        transforms.RandomVerticalFlip(),
+        transform = transforms.Compose([transforms.RandomVerticalFlip(),
                                         transforms.RandomHorizontalFlip(),
                                         transforms.ColorJitter(),
                                         transforms.ToTensor(),
                                        ])
 
     elif mode in 'val':
-        transform = transforms.Compose([transforms.RandomCrop((50, 50)),
-                                        transforms.ToTensor()])
+        transform = transforms.Compose([transforms.ToTensor()])
 
     elif mode in 'test':
         transform = transforms.Compose([transforms.ToTensor()])
@@ -28,22 +26,6 @@ def get_transform(mode):
 
     return transform
 
-class ImageDataSubset(Dataset):
-    def __init__(self, dataset, indices, mode):
-        self.dataset = dataset
-        self.indices = indices
-        self.transform = get_transform(mode)
-
-    def set_mode(self, mode):
-        self.transform = get_transform(mode)
-
-    def __getitem__(self, idx):
-        return self.transform(self.dataset[self.indices[idx]])
-
-    def __len__(self):
-        return len(self.indices)
-
-
 class Rescale():
     def __init__(self, scale):
         self.scale = scale
@@ -52,15 +34,47 @@ class Rescale():
         new_size = (int(x.size[0] * self.scale), int(x.size[1] * self.scale))
         return transforms.Resize(new_size)(x)
 
+class ImageDataSubset(Dataset):
+    def __init__(self, dataset, indices, mode='none', patch_size=-1):
+        self.dataset = dataset
+        self.indices = indices
+        self.patch_size = patch_size
+        self.transform = get_transform(mode)
+        self.patch_size = patch_size
+        if (patch_size > 0):
+            self.transform.transforms.insert(0, transforms.RandomCrop(patch_size))
+
+    def set_mode(self, mode):
+        self.transform = get_transform(mode)
+        if (self.patch_size > 0):
+            self.transform.transforms.insert(0, transforms.RandomCrop(self.patch_size))
+
+    def set_patch(self, patch_size):
+        if self.patch_size > 0:
+            self.transform.transforms.pop()
+
+        self.patch_size = patch_size
+
+        if (patch_size > 0):
+            self.transform.transforms.insert(0, transforms.RandomCrop(patch_size))
+
+    def __getitem__(self, idx):
+        return self.transform(self.dataset[self.indices[idx]])
+
+    def __len__(self):
+        return len(self.indices)
+
 class ImageDataset(Dataset):
-    def __init__(self, root_dirs, mode='none', store='ram', repeat=1, scale=-1):
+    def __init__(self, root_dirs, mode='none', store='ram', repeat=1, scale=-1, patch_size=-1):
         super().__init__()
         self.images = list()
         self.store = store
         self.repeat = repeat
+        self.scale = scale
+        self.patch_size = patch_size
 
         if type(root_dirs) != list:
-            root_dirs = list(root_dirs)
+            root_dirs = [root_dirs]
 
         for root_dir in root_dirs:
             for file_path in glob.glob(os.path.join(root_dir, '*.png')):
@@ -75,11 +89,33 @@ class ImageDataset(Dataset):
                     self.images.append(file_path)
 
         self.transform = get_transform(mode)
+        if (patch_size > 0):
+            self.transform.transforms.insert(0, transforms.RandomCrop(patch_size))
         if (scale > 0) and (store == 'disk'):
             self.transform.transforms.insert(0, Rescale(scale))
 
     def set_mode(self, mode):
         self.transform = get_transform(mode)
+        if (self.patch_size > 0):
+            self.transform.transforms.insert(0, transforms.RandomCrop(self.patch_size))
+        if (self.scale > 0) and (self.store == 'disk'):
+            self.transform.transforms.insert(0, Rescale(self.scale))
+
+    def set_patch(self, patch_size):
+
+        if (self.patch_size > 0):
+            if (self.scale > 0) and (self.store == 'disk'):
+                self.transform.transforms.pop(1)
+            else:
+                self.transform.transforms.pop(0)
+
+        self.patch_size = patch_size
+
+        if (patch_size > 0):
+            if (self.scale > 0) and (self.store == 'disk'):
+                self.transform.transforms.insert(1, transforms.RandomCrop(patch_size))
+            else:
+                self.transform.transforms.insert(0, transforms.RandomCrop(patch_size))
 
     def __len__(self):
         return int(len(self.images) * self.repeat)
