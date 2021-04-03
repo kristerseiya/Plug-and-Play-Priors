4858d10f6465c86760cf477ea212771664fc3c12
kristerseiya <>
2021-04-03 16:39:09 -0400

    add merge_bn

--- DnCNN/__init__.py
+++ DnCNN/__init__.py
@@ -1,4 +1,4 @@
 
 from .model import DnCNN
 from .run import train, validate, inference
-from .utils import load_dncnn
+from .utils import load_dncnn, merge_bn
--- DnCNN/utils.py
+++ DnCNN/utils.py
@@ -1,5 +1,6 @@
 
 import torch
+from torch import nn
 from . import model
 
 def single2tensor4(img):
@@ -14,38 +15,10 @@ def tensor2single(img):
 
 def load_dncnn(model_path, device=None):
 
-    # noise_level_img = 25             # noise level for noisy image
-    # model_name = 'dncnn_25'          # 'dncnn_15' | 'dncnn_25' | 'dncnn_50' | 'dncnn_gray_blind' | 'dncnn_color_blind' | 'dncnn3'
-    # testset_name = 'bsd68'           # test set, 'bsd68' | 'set12'
-    # need_degradation = True          # default: True
-    # x8 = False                       # default: False, x8 to boost performance
-    # show_img = False                 # default: False
-    #
-    # task_current = 'dn'       # 'dn' for denoising | 'sr' for super-resolution
-    # sf = 1                    # unused for denoising
-    # if 'color' in model_name:
-    #     n_channels = 3        # fixed, 1 for grayscale image, 3 for color image
-    # else:
     n_channels = 1        # fixed for grayscale image
-    # if model_name in ['dncnn_gray_blind', 'dncnn_color_blind', 'dncnn3']:
-    #     nb = 20               # fixed
-    # else:
+
     nb = 17               # fixed
-    # model_pool = 'model_zoo'  # fixed
-    # testsets = 'testsets'     # fixed
-    # results = 'results'       # fixed
-    # result_name = testset_name + '_' + model_name     # fixed
-    # border = sf if task_current == 'sr' else 0        # shave boader to calculate PSNR and SSIM
-    # model_path = os.path.join(model_pool, model_name+'.pth')
 
-
-    # if H_path == L_path:
-    #     need_degradation = True
-    # logger_name = result_name
-    # utils_logger.logger_info(logger_name, log_path=os.path.join(E_path, logger_name+'.log'))
-    # logger = logging.getLogger(logger_name)
-
-    # need_H = True if H_path is not None else False
     if device == None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -62,81 +35,36 @@ def load_dncnn(model_path, device=None):
     net = net.move(device)
 
     return net
-    # logger.info('Model path: {:s}'.format(model_path))
-    # number_parameters = sum(map(lambda x: x.numel(), model.parameters()))
-    # logger.info('Params number: {}'.format(number_parameters))
-#
-#     test_results = OrderedDict()
-#     test_results['psnr'] = []
-#     test_results['ssim'] = []
-#
-#     logger.info('model_name:{}, image sigma:{}'.format(model_name, noise_level_img))
-#     logger.info(L_path)
-#     L_paths = util.get_image_paths(L_path)
-#     H_paths = util.get_image_paths(H_path) if need_H else None
-#
-#     for idx, img in enumerate(L_paths):
-#
-#         # ------------------------------------
-#         # (1) img_L
-#         # ------------------------------------
-#
-#         img_name, ext = os.path.splitext(os.path.basename(img))
-#         # logger.info('{:->4d}--> {:>10s}'.format(idx+1, img_name+ext))
-#         img_L = util.imread_uint(img, n_channels=n_channels)
-#         img_L = util.uint2single(img_L)
-#
-#         if need_degradation:  # degradation process
-#             np.random.seed(seed=0)  # for reproducibility
-#             img_L += np.random.normal(0, noise_level_img/255., img_L.shape)
-#
-#         util.imshow(util.single2uint(img_L), title='Noisy image with noise level {}'.format(noise_level_img)) if show_img else None
-#
-#         img_L = util.single2tensor4(img_L)
-#         img_L = img_L.to(device)
-#
-#         # ------------------------------------
-#         # (2) img_E
-#         # ------------------------------------
-#
-#         if not x8:
-#             img_E = model(img_L)
-#         else:
-#             img_E = utils_model.test_mode(model, img_L, mode=3)
-#
-#         img_E = util.tensor2uint(img_E)
-#
-#         if need_H:
-#
-#             # --------------------------------
-#             # (3) img_H
-#             # --------------------------------
-#
-#             img_H = util.imread_uint(H_paths[idx], n_channels=n_channels)
-#             img_H = img_H.squeeze()
-#
-#             # --------------------------------
-#             # PSNR and SSIM
-#             # --------------------------------
-#
-#             psnr = util.calculate_psnr(img_E, img_H, border=border)
-#             ssim = util.calculate_ssim(img_E, img_H, border=border)
-#             test_results['psnr'].append(psnr)
-#             test_results['ssim'].append(ssim)
-#             logger.info('{:s} - PSNR: {:.2f} dB; SSIM: {:.4f}.'.format(img_name+ext, psnr, ssim))
-#             util.imshow(np.concatenate([img_E, img_H], axis=1), title='Recovered / Ground-truth') if show_img else None
-#
-#         # ------------------------------------
-#         # save results
-#         # ------------------------------------
-#
-#         util.imsave(img_E, os.path.join(E_path, img_name+ext))
-#
-#     if need_H:
-#         ave_psnr = sum(test_results['psnr']) / len(test_results['psnr'])
-#         ave_ssim = sum(test_results['ssim']) / len(test_results['ssim'])
-#         logger.info('Average PSNR/SSIM(RGB) - {} - PSNR: {:.2f} dB; SSIM: {:.4f}'.format(result_name, ave_psnr, ave_ssim))
-#
-# if __name__ == '__main__':
-#
-#     main()
+
+def merge_bn(model):
+    ''' Kai Zhang, 11/Jan/2019.
+    merge all 'Conv+BN' (or 'TConv+BN') into 'Conv' (or 'TConv')
+    based on https://github.com/pytorch/pytorch/pull/901
+    '''
+    prev_m = None
+    for k, m in list(model.named_children()):
+        if (isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d)) and (isinstance(prev_m, nn.Conv2d) or isinstance(prev_m, nn.Linear) or isinstance(prev_m, nn.ConvTranspose2d)):
+
+            w = prev_m.weight.data
+
+            if prev_m.bias is None:
+                zeros = torch.Tensor(prev_m.out_channels).zero_().type(w.type())
+                prev_m.bias = nn.Parameter(zeros)
+            b = prev_m.bias.data
+
+            invstd = m.running_var.clone().add_(m.eps).pow_(-0.5)
+            if isinstance(prev_m, nn.ConvTranspose2d):
+                w.mul_(invstd.view(1, w.size(1), 1, 1).expand_as(w))
+            else:
+                w.mul_(invstd.view(w.size(0), 1, 1, 1).expand_as(w))
+            b.add_(-m.running_mean).mul_(invstd)
+            if m.affine:
+                if isinstance(prev_m, nn.ConvTranspose2d):
+                    w.mul_(m.weight.data.view(1, w.size(1), 1, 1).expand_as(w))
+                else:
+                    w.mul_(m.weight.data.view(w.size(0), 1, 1, 1).expand_as(w))
+                b.mul_(m.weight.data).add_(m.bias.data)
+
+            del model._modules[k]
+        prev_m = m
+        merge_bn(m)
