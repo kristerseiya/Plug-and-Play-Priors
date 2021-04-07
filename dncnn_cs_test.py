16352afc76aa9d681835d4ad5f543ccb31ab1fb2
kristerseiya <>
2021-04-07 04:55:23 -0400

    fixed bug

--- dncnn_cs_test.py
+++ dncnn_cs_test.py
@@ -2,7 +2,10 @@
 import argparse
 import glob
 import os
+import sys
 import numpy as np
+from PIL import Image
+from tqdm import tqdm
 
 import noise
 import prox
@@ -14,7 +17,7 @@ parser.add_argument('--weights', help='path to weights', type=str, required=True
 parser.add_argument('--test_images', help='path to image', type=str, required=True)
 parser.add_argument('--trials', help='sample rate', type=int, default=20)
 parser.add_argument('--iter', help='number of iteration', type=int, default=100)
-parser.add_argument('--alphas', type=str, default='1:2000:20')
+parser.add_argument('--alphas', type=str, default='1:3.3:20')
 # parser.add_argument('--task', type=str, default='denoise')
 parser.add_argument('--noise', help='gaussian noise level', type=float, default=0)
 parser.add_argument('--save', help='file name to save results', type=str, default='results.txt')
@@ -30,15 +33,17 @@ if len(alphas)==2:
 else:
     n_points = int(alphas[2])
 
-alphas = np.logspace(start=int(alphas[0]), stop=int(alphas[1]), num=n_points)
+alphas = np.logspace(start=float(alphas[0]), stop=float(alphas[1]), num=n_points)
 
 img_path_list = list()
 for ext in ['png', 'jpg']:
     img_path_list += list(glob.glob(os.path.join(args.test_images, '*.'+ext)))
 
 
-samp_rates = np.linspace(0.1, 0.8. 0.05)
-log = np.zeros(len(img_path_list), len(samp_rates))
+samp_rates = np.arange(0.1, 0.8, 0.05)
+log = np.zeros((len(img_path_list), len(samp_rates)))
+
+pbar = tqdm(total=len(img_path_list), position=0, leave=False, file=sys.stdout)
 
 for i, img_path in enumerate(img_path_list):
 
@@ -48,11 +53,11 @@ for i, img_path in enumerate(img_path_list):
 
     for s, samp_rate in enumerate(samp_rates):
 
-        trial_exp = np.zeros([trials])
+        trial_exp = np.zeros([args.trials])
 
-        for t in range(trials):
+        for t in range(args.trials):
 
-            k = int(img.size * args.sample)
+            k = int(img.size * samp_rate)
             ri = np.random.choice(img.size, k, replace=False)
             mask = np.ones(img.shape, dtype=bool) * False
             mask.T.flat[ri] = True
@@ -65,15 +70,18 @@ for i, img_path in enumerate(img_path_list):
 
             for a, alpha in enumerate(alphas):
 
-                forward = prox.MSEwithMask(y, mask, alpha, input_shape=img.shape)
+                forward = prox.MSEwithMask(y, mask, img.shape, alpha)
                 optimizer = pnp.PnP_ADMM(forward, prior)
-                recon = optimizer.run(iter=args.iter, 0, return_value='x', verbose=False)
-                alpha_exp[a] = tools.compute_mse(image2uint8(img), image2uint8(recon))[0]
+                recon = optimizer.run(iter=args.iter, verbose=False, return_value='x')
+                alpha_exp[a] = tools.compute_mse(tools.image2uint8(img), tools.image2uint8(recon))[0]
 
             trial_exp[t] = np.min(alpha_exp)
 
         log[i, s] = trial_exp.mean()
 
+    pbar.update(1)
+
+tqdm.close(pbar)
 
 avg_mse = log.mean(axis=-1)
 
