50d354d09872f12de1490542bfa0a69d23678cd7
kristerseiya <>
2021-04-10 12:14:52 -0400

    detect number of layers

--- DnCNN/utils.py
+++ DnCNN/utils.py
@@ -16,9 +16,11 @@ def load_dncnn(model_path, device=None):
     # ----------------------------------------
     # load model
     # ----------------------------------------
+    state_dict = torch.load(model_path, map_location=device)
+    nb = len(state_dict.keys()) // 2
 
     net = model.DnCNN(in_nc=n_channels, out_nc=n_channels, nc=64, nb=nb, act_mode='R')  # use this if BN is not merged by utils_bnorm.merge_bn(model)
-    net.load_state_dict(torch.load(model_path, map_location=device), strict=True)
+    net.load_state_dict(state_dict, strict=True)
     if not torch.cuda.is_available():
         torch.set_flush_denormal(True)
     net.eval()
