781a5cab171d267174105226c1ebdfa6f06e1b57
kristerseiya <>
2021-04-10 10:11:15 -0400

    removed unused code

--- denoise.py
+++ denoise.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-from skimage.metrics import structural_similarity as skssim
 import argparse
 
 import tools
@@ -29,9 +28,6 @@ x = x.view(1, 1, *x.size())
 y = net(x)
 y = y.view(y.size(-2), y.size(-1))
 y = y.cpu().numpy()
-# tvnorm = prox.TVNorm(args.lambd)
-# tvnorm.set(1.)
-# recon = tvnorm.prox(noisy)
 
 noisy = tools.image2uint8(noisy)
 recon = tools.image2uint8(y)
@@ -39,12 +35,9 @@ recon = tools.image2uint8(y)
 mse, psnr = tools.compute_mse(img, recon)
 ssim = tools.ssim(img, recon).mean()
 msssim = tools.msssim(img, recon).mean()
-# ssim2 = skssim(img, recon, data_range=255)
 print('MSE: {:.5f}'.format(mse))
 print('PSNR: {:.5f}'.format(psnr))
 print('SSIM: {:.5f}'.format(ssim))
 print('MS-SSIM: {:.5f}'.format(msssim))
 
 tools.stackview([img, noisy, recon], width=20, method='Pillow')
-# noisy_img = Image.fromarray(tools.image_reformat(recon), 'L')
-# noisy_img.show()
